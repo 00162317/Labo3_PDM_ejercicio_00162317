28ca6dfa22a9282bdf1adfa53faf06935434ee97
Roberto Hernandez <hernandez.roberto014@gmail.com>
2019-03-22 23:08:36 -0600

    Muestra datos en otra pantalla

--- app/src/main/java/com/example/mostrardatos/Main2Activity.java
+++ app/src/main/java/com/example/mostrardatos/Main2Activity.java
@@ -19,8 +19,7 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        Button share = findViewById(R.id.btnShares);
-        mostrando();
+          mostrando();
 
     }
     private void mostrando(){
